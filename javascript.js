d653f003d36f6d87b5566a5b1eb5af6e30fb783e
faithfully04 <faithfully04@yahoo.com>
2022-06-26 19:11:49 +0100

    commented out the console log

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ const myName = "Faith";
 const myHeight = '178cm';
 const myCountry = 'Nigeria';
 
-console.log(myName);
-console.log(myHeight);
-console.log(myCountry);
+//console.log(myName);
+//console.log(myHeight);
+//console.log(myCountry);
 
 alert('Hello! My name is Faith. I am 5 feet 8 inches tall and I am from Nigeria.');
 
